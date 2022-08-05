11f365f0307a272fdcb374f0d290e9cb527dfa66
michalisd <DimitriouMich@gamail.com>
2022-08-05 08:20:31 +0200

    formatting

--- trigrams.go
+++ trigrams.go
@@ -25,9 +25,9 @@ func check(e error) {
 	}
 }
 
-func generatetext (start string, nwordsneeded int, trigrams []string) string{
+func generatetext(start string, nwordsneeded int, trigrams []string) string {
 	//Finds the matching trigrams and randomly selects one to continue, does this recursively until desired number of words is reached.
-	nextcanditates:=make([]string,0)
+	nextcanditates := make([]string, 0)
 	words := strings.Fields(start)
 	lastwords := words[len(words)-2] + " " + words[len(words)-1]
 	for _, trigram := range trigrams {
@@ -37,14 +37,14 @@ func generatetext (start string, nwordsneeded int, trigrams []string) string{
 			nextcanditates = append(nextcanditates, nextword)
 		}
 	}
-	if len(nextcanditates)>1 {
-		randindx := rand.Intn(len(nextcanditates)-1)
+	if len(nextcanditates) > 1 {
+		randindx := rand.Intn(len(nextcanditates) - 1)
 		start = start + " " + nextcanditates[randindx]
 	} else {
 		start = start + " " + nextcanditates[0]
 	}
 	if len(strings.Fields(start)) < nwordsneeded {
-		start = generatetext(start,nwordsneeded,trigrams)
+		start = generatetext(start, nwordsneeded, trigrams)
 	}
 	return start
 
@@ -52,49 +52,51 @@ func generatetext (start string, nwordsneeded int, trigrams []string) string{
 
 func filereader(path string) []string {
 	//Reads a file and returns a slice with all the trigrams in that file.
-			//Open the input file for reading
-			file, err := os.Open(path)
-			check(err)
-			defer file.Close()
-			//store the string into Trigrams
-			trigram := ""
-			trigrams := make([]string, 0)
-			scanner := bufio.NewScanner(file)
-			scanner.Split(bufio.ScanWords)
-			//scan the first two words
-			scanner.Scan()
-			trigram =scanner.Text()
-			scanner.Scan()
-			trigram = trigram + " "+ scanner.Text()
-			//build a trigram for every word after the second
-			for scanner.Scan() {
-				trigram = trigram + " " + scanner.Text()
-				trigrams = append(trigrams, trigram)
-				trigram = trigram[strings.Index(trigram, " ")+1:]
-			}
-			check(scanner.Err())
-			return trigrams
-		}
-func initoutstr(trigrams []string) string {
-			//Randomly selects a trigram that starts with capital leter to start the generation
-			var start string
-			startcanditates:=make([]string,0)
-			r, _ := regexp.Compile("[A-Z]")
-			for i :=0; i<len(trigrams); i++ {
-				trigram := trigrams[i]
-				if r.MatchString(trigram[:1]){
-					startcanditates = append(startcanditates, trigram)
-				}
-			}
-			rand.Seed(time.Now().UnixNano())
-			if len(startcanditates)>1{
-				randindx := rand.Intn(len(startcanditates)-1)
-				start = startcanditates[randindx]
-			} else {
-				start = startcanditates[0]
-			}
-			return start
+	//Open the input file for reading
+	file, err := os.Open(path)
+	check(err)
+	defer file.Close()
+	//store the string into Trigrams
+	trigram := ""
+	trigrams := make([]string, 0)
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanWords)
+	//scan the first two words
+	scanner.Scan()
+	trigram = scanner.Text()
+	scanner.Scan()
+	trigram = trigram + " " + scanner.Text()
+	//build a trigram for every word after the second
+	for scanner.Scan() {
+		trigram = trigram + " " + scanner.Text()
+		trigrams = append(trigrams, trigram)
+		trigram = trigram[strings.Index(trigram, " ")+1:]
+	}
+	check(scanner.Err())
+	return trigrams
 }
+
+func initoutstr(trigrams []string) string {
+	//Randomly selects a trigram that starts with capital leter to start the generation
+	var start string
+	startcanditates := make([]string, 0)
+	r, _ := regexp.Compile("[A-Z]")
+	for i := 0; i < len(trigrams); i++ {
+		trigram := trigrams[i]
+		if r.MatchString(trigram[:1]) {
+			startcanditates = append(startcanditates, trigram)
+		}
+	}
+	rand.Seed(time.Now().UnixNano())
+	if len(startcanditates) > 1 {
+		randindx := rand.Intn(len(startcanditates) - 1)
+		start = startcanditates[randindx]
+	} else {
+		start = startcanditates[0]
+	}
+	return start
+}
+
 func main() {
 	nwordsneeded := 50
 	if !validateInput(os.Args) {
@@ -102,6 +104,6 @@ func main() {
 	}
 	trigrams := filereader(os.Args[1])
 	start := initoutstr(trigrams)
-	fmt.Println(generatetext(start, nwordsneeded,trigrams))
+	fmt.Println(generatetext(start, nwordsneeded, trigrams))
 
 }
